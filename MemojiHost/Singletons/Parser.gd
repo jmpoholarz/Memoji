8e7f91f884c592d319dbe8af9eb939f28fb6eb43
jmpoholarz <jmpoholarz@gmail.com>
2019-02-06 18:35:25 -0500

    Added Networking code skeleton to decode messages from the server

--- MemojiHost/Singletons/Parser.gd
+++ MemojiHost/Singletons/Parser.gd
@@ -1,4 +1,31 @@
 extends Node
 
-func parseMessage(message):
-	return message
+func encodeMessage(dictionary):
+	"""
+	Converts a dictionary message into Json format to be sent to the server
+	Arguments:
+		dictionary - the message in dictionary format
+	Returns:
+		json - the message in Json format
+	"""
+	var json = JSON.print(dictionary)
+	return json
+
+func decodeMessage(json):
+	"""
+	Converts a Json message from the server back into usable dictionary format
+	Arguments:
+		json - the message in Json format
+	Returns:
+		dictionary - the message in Dictionary format
+	"""
+	var dictionary = JSON.parse(json).result
+	return dictionary
+
+func testJsonConversions():
+	var d1 = {"apple":1, "banana":"orange"}
+	var j1 = JSON.print(d1)
+	
+	var d2 = JSON.parse(j1).result
+	print(d2)
+	
