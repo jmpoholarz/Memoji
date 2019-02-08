198bb2a2a6e4466ee948d0cc8477deabfb742778
jmpoholarz <jmpoholarz@gmail.com>
2019-02-08 17:09:17 -0500

    Added Logger write functions

--- MemojiHost/Singletons/Logger.gd
+++ MemojiHost/Singletons/Logger.gd
@@ -11,17 +11,39 @@ func writeLaunchDetails():
 	if success != 0:
 		print("Error opening log file.  Creating new file.")
 		errorFile.open("user://log.txt", File.WRITE)
+	errorFile.seek_end()
 	var timeDict = OS.get_datetime()
-	errorFile.store_line("- - - Memoji Host - - -")
-	errorFile.store_line("Running at " + getTimeString())
-	errorFile.store_line("- - - - - - - - - - - -")
+	errorFile.store_line("- - - - - Memoji Host - - - - -\r\n")
+	errorFile.store_line("Running at " + getDateTimeString() + "\r\n")
+	errorFile.store_line("- - - - - - - - - - - - - - - -\r\n")
 	errorFile.close()
+
+func getDateTimeString():
+	var timeDict = OS.get_datetime()
+	var minute = timeDict["minute"]
+	if minute < 10:
+		minute = "0" + minute
+	var timeString = str(timeDict["hour"]) + ":" + str(minute) + " on " +  \
+		str(timeDict["month"]) + "/" + str(timeDict["day"]) + "/" + str(timeDict["year"])
+	return timeString
 
 func getTimeString():
 	var timeDict = OS.get_datetime()
 	var minute = timeDict["minute"]
 	if minute < 10:
 		minute = "0" + minute
-	var timeString = str(timeDict["hour"]) + ":" + str(minute) + "on" +  \
-		str(timeDict["month"]) + "/" + str(timeDict["day"]) + str(timeDict["year"])
+	var timeString = str(timeDict["hour"]) + ":" + str(minute)
 	return timeString
+
+func writeLine(string):
+	errorFile.open("user://log.txt", File.READ_WRITE)
+	errorFile.seek_end()
+	errorFile.store(string + "\r\n")
+	errorFile.close()
+
+func write(string):
+	errorFile.open("user//log.txt", File.READ_WRITE)
+	errorFile.seek_end()
+	errorFile.store(string)
+	errorFile.close()
+
