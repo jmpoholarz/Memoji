4894e8d28ff2660a0e9d80df4aa91f5a3e08e11f
octosquidport <deluns@gmail.com>
2019-02-17 15:56:25 -0500

    progress on handling dynamic player updates

--- MemojiHost/LobbyDisplays/LobbyScreen.gd
+++ MemojiHost/LobbyDisplays/LobbyScreen.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const PlayerClass = preload("res://Player.gd")
+
 onready var p1 = $Foreground/Content/Lines/TopLine/Statuses/PlayerStatus/P1
 onready var p2 = $Foreground/Content/Lines/TopLine/Statuses/PlayerStatus/P2
 onready var p3 = $Foreground/Content/Lines/TopLine/Statuses/PlayerStatus/P3
@@ -14,6 +16,19 @@ onready var pDisplays = [p1, p2, p3, p4, p5, p6, p7, p8] # GUI representing each
 var avatarList = [] # Stores the avatars, indexed by Player.AvatarID
 var playerInfo = [] # Stores the playerID, username, and avatarIndex of each player
 
+### DEBUG/TESTING ###
+func _debug():
+	var ids = [400025, 2111079, 90001]
+	
+	randomize()
+	
+	for x in ids:
+		add_player_id(x)
+		update_player_display(x, "%d" % x, randi() % 8)
+		pass
+	
+	return
+
 func _ready():
 	avatarSetup()
 	
@@ -21,12 +36,15 @@ func _ready():
 	
 	for x in range(0, 8):
 		pDisplays[x].update_player("Player %d" % (x + 1), avatarList[x])
+		pDisplays[x].hide()
+	
+	_debug()
 	
 	### DEBUG ###
 	var playerTest = self.get_child(1)
 	print (playerTest)
 	
-	pass
+	return
 
 func _process(delta):
 	
@@ -43,9 +61,25 @@ func avatarSetup(): # loads the avatars in use
 func update_player_display(playerID, username, avatarIndex):
 	
 	# find player
-	var playerIndex = 0
+	var index = playerInfo.find(playerID)
 	
-	# call P_Status node's update_player() function
-	# 
+	if (index == -1): # no player with that ID was found
+		return -1
+		
+	pDisplays[index].update_player(username, avatarList[avatarIndex])
 	
+	return 0
 	pass
+
+# Adds a player into the playerInfo array, 
+func add_player_id(playerID):
+	var index = playerInfo.size()
+	
+	if (index < 8):
+		playerInfo.append(playerID)
+		pDisplays[index].show()
+	else:
+		return 1 # TODO: put in audience etc.
+	
+	return 0
+	
