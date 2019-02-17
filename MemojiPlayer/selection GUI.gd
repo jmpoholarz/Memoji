1593e8217a6d047a82e5d6b2a155173686dfeec1
Andrew <amring777@gmail.com>
2019-02-17 16:08:23 -0500

    submit button gets texture id and name

--- MemojiPlayer/selection GUI.gd
+++ MemojiPlayer/selection GUI.gd
@@ -1,6 +1,7 @@
 extends Container
 
 signal change_icon(iconId)
+signal change_text(text)
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
@@ -9,7 +10,10 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	var textRect = get_node("MarginContainer/HBoxContainer/VBoxContainer/VBoxContainer/HBoxContainer/TextureRect")
+	var textSubmit = get_node("MarginContainer/HBoxContainer/VBoxContainer/VBoxContainer3/MarginContainer/TouchScreenButton")
 	connect("change_icon", textRect, "on_change_icon")
+	connect("change_icon", textSubmit, "on_change_icon")
+	connect("change_text", textSubmit, "on_change_text")
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
@@ -19,3 +23,8 @@ func _ready():
 
 func _on_Button_pressed(id):
 	emit_signal("change_icon", id)
+
+
+func _on_TextEdit_text_changed():
+	var textBox = get_node("MarginContainer/HBoxContainer/VBoxContainer/VBoxContainer/HBoxContainer/TextEdit")
+	emit_signal("change_text", textBox.text)
