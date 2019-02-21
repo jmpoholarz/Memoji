cb23f71473a9c9d4d7b5bf59d20ca4ddbdd1b66a
jone1268 <akatuesday101@gmail.com>
2019-02-21 17:32:09 -0500

    WIP: Errors with message sending.

--- MemojiServer/server.js
+++ MemojiServer/server.js
@@ -111,8 +111,7 @@ const server = net.createServer(socket => {
       console.warn('Message does not contain letterCode');
       console.warn(err);
       const res = {
-        "messageType": 100,
-        "message": data.toString()
+        "messageType": 100
       };
       writeToFile(error_log, 'Message does not contain letterCode');
       send(socket, JSON.stringify(res));
@@ -144,9 +143,9 @@ const server = net.createServer(socket => {
             if(id === -1){
               console.log('Player already connected to host.');
               writeToFile(error_log, `Player has already connected to host. Do not add to host again.`);
-              return;
+            } else {
+              writeToFile(server_log, `Player: [${id}] joined Host - ${letterCode}`);
             }
-            writeToFile(server_log, `Player: [${id}] joined Host - ${letterCode}`);
           } else {
             // Host lobby full, join as audience member
             var id = handleAudienceConn(letterCode, socket);
@@ -238,7 +237,6 @@ const server = net.createServer(socket => {
     };
     writeToFile(error_log, 'Error. Send 100 back to server. Requesting message again.');
     send(socket, JSON.stringify(res));
-    server.listen(port, '127.0.0.1');
   });
 });
 
