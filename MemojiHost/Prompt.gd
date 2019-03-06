a5f7cd1fc150482232e66138796572a5833a83e1
jmpoholarz <jmpoholarz@gmail.com>
2019-03-05 22:14:47 -0500

    Added unsorted emojis; added Prompt getters and setters, some PromptManager functions

--- MemojiHost/Prompt.gd
+++ MemojiHost/Prompt.gd
@@ -1,30 +1,83 @@
 extends Node
 
-var promptID
-var text
-var playerAnswers = []
-var playerVotes = []
+var prompt_id
+var prompt_text
+var player_answers = []
+var player_votes = []
 
 class Answer:
-	var playerID
+	var player_id
 	var emojis
 
 class Vote:
-	var playerID
-	var voteID
+	var player_id
+	var vote_index
 
-func getPlayers():
+# Getters
+
+func get_prompt_id():
+	return prompt_id
+
+func get_prompt_text():
+	return prompt_text
+
+func get_answers():
+	return player_answers
+
+func get_votes():
+	return player_votes
+
+func get_answer_from_player(player_id):
+	for answer in player_answers:
+		if answer.player_id == player_id:
+			return answer.emojis
+
+func get_vote_from_player(player_id):
+	for vote in player_votes:
+		if vote.player_id == player_id:
+			return vote.vote_index
+
+func get_number_of_votes_for_answer(answer_index):
+	counter = 0
+	for vote in player_votes:
+		if vote.vote_index == answer_index:
+			counter += 1
+	return counter
+
+func get_players_who_answered():
 	var players = []
-	
-	for ans in playerAnswers:
-		players.append(ans.playerID)
-		pass
-	
+	for answer in player_answers:
+		players.append(ans.player_id)
 	return players
-	
-func insertAnswer(playerID, emojiArray):
-	var ans = Answer.new()
-	ans.playerID = playerID
-	ans.emojis = emojiArray
-	
-	playerAnswers.append(ans)
+
+func get_players_who_voted():
+	var players = []
+	for vote in player_votes:
+		players.append(vote.player_id)
+	return players
+
+# Setters
+
+func set_prompt_id(prompt_id):
+	self.prompt_id = prompt_id
+
+func set_prompt_text(prompt_text):
+	self.prompt_text = prompt_text
+
+func add_player_answer(player_id, emojis):
+	# Create object to hold answer
+	var answer_obj = Answer.new()
+	answer_obj.player_id = player_id
+	answer_obj.emojis = emojis
+	# Store answer and add as a child
+	player_answers.append(answer_obj)
+	add_child(answer_obj)
+
+func add_player_vote(player_id, vote_index):
+	# Create object to hold vote
+	var vote_obj = Vote.new()
+	vote_obj.player_id = player_id
+	vote_obj.vote_index = vote_index
+	# Store vote and add as a child
+	player_votes.append(vote_obj)
+	add_child(vote_obj)
--- MemojiHost/PromptManager.gd
+++ MemojiHost/PromptManager.gd
@@ -1,22 +1,48 @@
 extends Node
 
-const TOTAL_QUESTIONS = 4
+var prompt_scene = preload("res://Prompt.tscn")
 
-var active_prompts = []
+const TOTAL_QUESTIONS = 21
+
+var active_prompt_ids = []
+var active_prompts = {}
 
 func _ready():
 	__unit_test_get_new_prompt()
 	pass
 
-func setAnswer(playerID, promptID):
-	pass
+func setAnswer(prompt_id, player_id, answer):
+	active_prompts[prompt_id].add_player_answer(player_id, answer)
+
+func set_vote(prompt_id, player_id, answer_index):
+	active_prompts[prompt_id].add_player_vote(player_id, answer_index)
+
+func get_answers_to_prompt(prompt_id):
+	var answers = []
+	for answer in active_prompts[prompt_id].get_answers():
+		answers += answer.emojis
+
+
+
+func create_prompt():
+	# Get data stored in prompt
+	var prompt_data = _get_new_prompt()
+	var prompt_id = prompt_data[0]
+	var prompt_text = prompt_data[1]
+	# Create prompt object and add as child
+	var prompt_obj = prompt_scene.instance()
+	prompt_obj.set_prompt_id(prompt_id)
+	prompt_obj.set_prompt_text(prompt_text)
+	add_child(prompt_obj)
+	# Return the prompt object if needed
+	return prompt_obj
 
 func _get_new_prompt(prompt_number = -1):
-	if active_prompts.size() == TOTAL_QUESTIONS:
+	if active_prompt_ids.size() == TOTAL_QUESTIONS:
 		print("Failed to generate new prompt as all prompts have been chosen.  Resetting.")
-		active_prompts.clear()
+		active_prompt_ids.clear()
 	# Generate random number
-	while prompt_number < 0 || prompt_number >= TOTAL_QUESTIONS || prompt_number in active_prompts:
+	while prompt_number < 0 || prompt_number >= TOTAL_QUESTIONS || prompt_number in active_prompt_ids:
 		prompt_number = randi() % TOTAL_QUESTIONS
 	# Read in prompt from that line of the file
 	var prompt_text = ""
@@ -33,7 +59,7 @@ func _get_new_prompt(prompt_number = -1):
 	f.close()
 	print(str(prompt_number) + ":::" + prompt_text)
 	# Add chosen prompts to activePrompts so it isn't chosen again
-	active_prompts.append(prompt_number)
+	active_prompt_ids.append(prompt_number)
 	return prompt_text
 
 
@@ -53,3 +79,9 @@ func __unit_test_get_new_prompt():
 	_get_new_prompt(-2)
 	_get_new_prompt(1)
 	_get_new_prompt(14)
+	
+	var prompt_obj = create_prompt()
+	prompt_obj.add_player_answer(333, "abcd")
+	print(str(prompt_obj.get_players()[0]))
+	
+	## TODO test more getters
