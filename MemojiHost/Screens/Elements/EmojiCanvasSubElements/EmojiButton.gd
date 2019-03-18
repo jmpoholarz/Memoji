449114e0b62101f05f3daa8034c2d2482168e8ef
jmpoholarz <jmpoholarz@gmail.com>
2019-03-18 18:59:55 -0400

    Added EmojiCanvas elements to the Host Screens/Elements folder

---
+++ MemojiHost/Screens/Elements/EmojiCanvasSubElements/EmojiButton.gd
@@ -0,0 +1,12 @@
+extends Button
+
+signal emoji_button_pressed(id)
+
+var _emoji_id = -1
+
+func set_emoji_id(id):
+	_emoji_id = id
+
+func _on_EmojiButton_pressed():
+	emit_signal("emoji_button_pressed", _emoji_id)
+	#print(_emoji_id)
