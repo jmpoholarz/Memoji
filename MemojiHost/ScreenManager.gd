41ba024fb8f6b45a99ccd5757169c7a218948603
amring777 <amring777@gmail.com>
2019-03-20 15:48:57 -0400

    add results screen to screen manager

--- MemojiHost/ScreenManager.gd
+++ MemojiHost/ScreenManager.gd
@@ -4,6 +4,7 @@ var titleScreenScene = preload("res://Screens/FirstTitle.tscn")
 var setupScreenScene = preload("res://Screens/Setup.tscn")
 var lobbyScreenScene = preload("res://Screens/LobbyDisplays/LobbyScreen.tscn")
 var waitScreenScene = preload("res://Screens/WaitScreen.tscn")
+var resultsScreenScene = preload("res://Screens/HostResultsScreen.tscn")
 
 signal connectToServer()
 signal sendMessageToServer(msg)
@@ -36,6 +37,8 @@ func changeScreenTo(screen):
 			currentScreenInstance.connect("updateGameState", self, "forwardGameState")
 		GlobalVars.WAIT_SCREEN:
 			currentScreenInstance = waitScreenScene.instance()
+		GlobalVars.RESULTS_SCREEN:
+			currentScreenInstance = resultsScreenScene.instance()
 	
 	if (currentScreenInstance != null):
 		currentScreenInstance.connect("messageServer", self, "forwardMessage")
