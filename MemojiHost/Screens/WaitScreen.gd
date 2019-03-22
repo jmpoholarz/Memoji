02ad1afbb5a82c253b3c7c00466f74b8c8c5b6a2
jone1268 <akatuesday101@gmail.com>
2019-03-22 18:13:46 -0400

    Revert "Fix Waiting Screen timer"
    This reverts commit e23f75f0a78011129b6e9ab8916d5e05368dd4b2.

--- MemojiHost/Screens/WaitScreen.gd
+++ MemojiHost/Screens/WaitScreen.gd
@@ -8,30 +8,28 @@ signal messageServer(msg)
 
 var timer
 var _TimerLabel
-var _Timer
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	_TimerLabel = $MarginContainer/TimerLabel
-	_Timer = $Timer
+	_TimerLabel = $MarginContainer/Timer
+	timer = Timer.new()
+	timer.connect("timeout", self, "_on_timer_timeout")
+	timer.wait_time(60)
+	add_child(timer)
+	timer.start()
 	pass
 
-#func _process(delta):
+func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
-#	_TimerLabel.text = str(timer.time_left)
-#	pass
+	_TimerLabel.text = timer.time_left
+	pass
 
 # Add timer node on start
 # Make label to display timer value
 
-func _on_Timer_timeout():
-	_TimerLabel.text = str(int(_TimerLabel.text)-1)
-	if int(_TimerLabel.text) == 0:
-		_Timer.stop()
-		var message = {
-			"messageType": 320
-		}
-		emit_signal("messageServer", message)
-	pass # replace with function body
+func _on_timer_timeout():
+	print("TIMER OVER")
+	var message = {"messageType": 320}
+	emit_signal("messageServer", message)
