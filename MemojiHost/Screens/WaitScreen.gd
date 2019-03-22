ed25313b9a2c7d98f7630d76f4b09222951f1314
jone1268 <akatuesday101@gmail.com>
2019-03-22 18:17:03 -0400

    Fix merge issue.

--- MemojiHost/Screens/WaitScreen.gd
+++ MemojiHost/Screens/WaitScreen.gd
@@ -6,30 +6,28 @@ extends Node
 
 signal messageServer(msg)
 
-var timer
 var _TimerLabel
+var _Timer
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	_TimerLabel = $MarginContainer/Timer
-	timer = Timer.new()
-	timer.connect("timeout", self, "_on_timer_timeout")
-	timer.wait_time(60)
-	add_child(timer)
-	timer.start()
+	_TimerLabel = $MarginContainer/TimerLabel
+	_Timer = $Timer
 	pass
 
-func _process(delta):
+#func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
-	_TimerLabel.text = timer.time_left
-	pass
+#	pass
 
 # Add timer node on start
 # Make label to display timer value
 
-func _on_timer_timeout():
-	print("TIMER OVER")
-	var message = {"messageType": 320}
-	emit_signal("messageServer", message)
+func _on_Timer_timeout():
+	_TimerLabel.text = str(int(_TimerLabel.text)-1)
+	if int(_TimerLabel) == 0:
+		_Timer.stop()
+		var message = {"messageType": 320}
+		emit_signal("messageServer", message)
+	pass # replace with function body
