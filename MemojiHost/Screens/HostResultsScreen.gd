af9d33d2ea943b6076224e24d51c98031af4526b
Andrew <amring777@gmail.com>
2019-03-22 15:57:26 -0400

    set visible to false when not a voter

--- MemojiHost/Screens/HostResultsScreen.gd
+++ MemojiHost/Screens/HostResultsScreen.gd
@@ -37,12 +37,18 @@ func calculateTotals(ID, votes, audiencePercent):
 	
 func displayVoters(votes):
 	#recieve who voted for each answer and display appropriately
-	var currentNode = getNode("MarginContainer/Rows/Voters/VotersRight/PlayerIcon1")
+	var voterLoc = "MarginContainer/Rows/Voters/"
+	var currentNode
 	for x in range(0, votes.size()):
 		if(votes[x] == 1):
-			currentNode = getNode("MarginContainer/Rows/Voters/VotersLeft/PlayerIcon" + str(x+1))
+			currentNode = getNode(voterLoc + "VotersLeft/PlayerIcon" + str(x+1))
 			currentNode.visible = true
 		elif(votes[x] == 2):
-			currentNode = getNode("MarginContainer/Rows/Voters/VotersRight/PlayerIcon" + str(x+1))
+			currentNode = getNode(voterLoc + "VotersRight/PlayerIcon" + str(x+1))
 			currentNode.visible = true
+		else:
+			currentNode = getNode(voterLoc + "VotersLet/PlayerIcon" + str(x+1))
+			currentNode.visible = false
+			currentNode = getNode(voterLoc + "VotersRight/PlayerIcon" + str(x+1))
+			currentNode.visible = false
 	return
