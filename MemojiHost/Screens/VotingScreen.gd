924505418fcbf93c635f5dddd5a65fa23bb86825
octosquidport <deluns@gmail.com>
2019-03-24 14:53:44 -0400

    Voting Screen progress

---
+++ MemojiHost/Screens/VotingScreen.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+signal(
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _on_Timer_timeout():
+	_TimerLabel.text = str(int(_TimerLabel.text)-1)
+	if int(_TimerLabel) == 0:
+		_Timer.stop()
+		var message = {"messageType": MESSAGE_TYPES.HOST_TIME_UP}
+		emit_signal("messageServer", message)
+	pass # replace with function body
