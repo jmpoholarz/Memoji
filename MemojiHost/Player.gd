452ba7e247322bf83ecd52c67a1c956d6ca7f2aa
octosquidport <deluns@gmail.com>
2019-04-05 17:11:08 -0400

    added storage of prompts to player obj

--- MemojiHost/Player.gd
+++ MemojiHost/Player.gd
@@ -2,3 +2,13 @@ var playerID
 var username
 var avatarID
 var isPlayer
+var currentPromptIDs = []
+
+func clear_prompts():
+	currentPromptIDs.clear()
+	
+func add_promptID(id):
+	currentPromptIDs.append(id)
+
+func get_promptIDs():
+	currentPromptIDs
