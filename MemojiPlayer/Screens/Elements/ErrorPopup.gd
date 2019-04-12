db7eb93b2e8a71f8a476a67f234ea42267e1f385
jmpoholarz <jmpoholarz@gmail.com>
2019-04-12 16:38:09 -0400

    Fixed error popups not updating text at runtime but broke updating text in editor - idk how to fix this atm

--- MemojiPlayer/Screens/Elements/ErrorPopup.gd
+++ MemojiPlayer/Screens/Elements/ErrorPopup.gd
@@ -9,7 +9,11 @@ func _ready():
 	_Label.text = label_text
 
 func _update_text(new_text):
+	if !is_inside_tree():
+		return
 	_Label.text = new_text
 
 func _get_text():
+	if !is_inside_tree():
+		return
 	return _Label.text
