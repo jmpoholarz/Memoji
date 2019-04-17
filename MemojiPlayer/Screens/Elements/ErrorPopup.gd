15bf788d6f59bc298b27f34ca04e1491a87efd7b
jmpoholarz <jmpoholarz@gmail.com>
2019-04-17 15:33:09 -0400

    Started grey window theme

--- MemojiPlayer/Screens/Elements/ErrorPopup.gd
+++ MemojiPlayer/Screens/Elements/ErrorPopup.gd
@@ -1,15 +1,12 @@
 tool
 extends PopupPanel
 
-export(String) var label_text = "Sample text." setget _set_label_text, _get_label_text
+export(String) var label_text = "Sample text."
 
 onready var _Label = $Label
 
-func _set_label_text(new_text):
-	label_text = new_text
-
-func _get_label_text():
-	return label_text
+func _ready():
+	_Label.text = label_text
 
 func _on_ErrorPopup_about_to_show():
 	_Label.text = label_text
