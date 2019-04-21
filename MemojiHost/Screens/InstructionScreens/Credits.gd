2f8f9b5a51423c39791fa2cd48226e03b49ec05b
amring777 <amring777@gmail.com>
2019-04-21 01:09:39 -0400

    credits functions

--- MemojiHost/Screens/InstructionScreens/Credits.gd
+++ MemojiHost/Screens/InstructionScreens/Credits.gd
@@ -20,9 +20,9 @@ func _ready():
 
 func _on_SameButton_pressed():
 	#restart the same game
-	pass # replace with function body
+	emit_signal("restart")
 
 
 func _on_NewButton_pressed():
 	#begin a new game
-	pass # replace with function body
+	emit_signal("newGame")
