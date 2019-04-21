a20505e191925f65047f53124e1086f7cd454b2e
amring777 <amring777@gmail.com>
2019-04-21 01:08:46 -0400

    credits script

---
+++ MemojiHost/Screens/InstructionScreens/Credits.gd
@@ -0,0 +1,28 @@
+extends Panel
+
+signal restart()
+signal newGame()
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_SameButton_pressed():
+	#restart the same game
+	pass # replace with function body
+
+
+func _on_NewButton_pressed():
+	#begin a new game
+	pass # replace with function body
