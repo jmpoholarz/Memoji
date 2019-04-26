afe136ca3bfb2d7e0c138e1c0a0111812d5914df
jone1268 <jonesmtuesday@gmail.com>
2019-04-25 23:18:19 -0400

    Fix disconnect button functionality on waiting for game to start screen. 😂

--- MemojiPlayer/Screens/WaitingForGameStartScreen.gd
+++ MemojiPlayer/Screens/WaitingForGameStartScreen.gd
@@ -4,21 +4,16 @@ signal sendMessage(msg)
 signal disconnectFromHost()
 signal changeScreen(screen)
 
-var roomCode = "????"
-
 onready var _BackButton = $VBoxContainer/HBoxContainer/VBoxContainer/BackButton
 
 func hide_back_button():
 	_BackButton.visible = false
 
-func new_room_code(code):
-	roomCode = code;
-
 func _on_BackButton_pressed():
 	emit_signal("changeScreen", 2)
 
 func _on_DisconnectButton_pressed():
-	var msg = {"messageType": MESSAGE_TYPES.PLAYER_DISCONNECTED, "letterCode":roomCode}
+	var msg = {"messageType": MESSAGE_TYPES.PLAYER_DISCONNECTED}
 	emit_signal("sendMessage", msg)
 	emit_signal("disconnectFromHost")
 	emit_signal("changeScreen", 1)
